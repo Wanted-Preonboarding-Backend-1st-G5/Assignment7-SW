43bc1e2aa505d2c2b0edef791a4a7debf76e2282
sw-develop <psw00719@naver.com>
2021-11-26 13:58:47 +0900

    Add: setting for springboot - build: Gradle - language: Java 11 - version: SpringBoot 2.5.7 - packaging: Jar

---
+++ src/test/java/com/project/cardoc/CardocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cardoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
